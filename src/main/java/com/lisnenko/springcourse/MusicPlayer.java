5ec3999b97f2a03fa63770adbd0b3b1aa1ed87a8
Vlad Lisnenko <lisnenkov777@gmail.com>
2021-11-04 10:45:09 +0200

    using Dependency Injection(DI) with the help Setter

--- src/main/java/com/lisnenko/springcourse/MusicPlayer.java
+++ src/main/java/com/lisnenko/springcourse/MusicPlayer.java
@@ -3,11 +3,11 @@ package com.lisnenko.springcourse;
 public class MusicPlayer {
     private Music music;
 
-    public MusicPlayer(Music music){
-        this.music = music;
-    }
+    public MusicPlayer() { }
 
-    public void playMusic(){
-        System.out.println("Playing: " +  music.getSong());
-    }
+    //IoC
+    public MusicPlayer(Music music){ this.music = music;}
+    public void setMusic(Music music){ this.music = music;}
+
+    public void playMusic(){ System.out.println("Playing: " +  music.getSong());}
 }
