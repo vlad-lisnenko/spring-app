5f68a6197989429c2f4cdc6d92cc55ec42c5b73d
Vlad Lisnenko <lisnenkov777@gmail.com>
2021-11-05 11:28:13 +0200

    using @Qualifier

--- src/main/java/com/lisnenko/springcourse/classes/MusicPlayer.java
+++ src/main/java/com/lisnenko/springcourse/classes/MusicPlayer.java
@@ -1,26 +1,27 @@
 package com.lisnenko.springcourse.classes;
 
-import com.lisnenko.springcourse.classes.genre.ClassicalMusic;
-import com.lisnenko.springcourse.classes.genre.JazzMusic;
-import com.lisnenko.springcourse.classes.genre.RockMusic;
+import com.lisnenko.springcourse.interfaces.Music;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
-    private JazzMusic jazzMusic;
+    private final Music music1;
+    private final Music music2;
+    private final Music music3;
 
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic, JazzMusic jazzMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
-        this.jazzMusic = jazzMusic;
+    public MusicPlayer(@Qualifier("rockMusic") Music music1,
+                       @Qualifier("classicalMusic") Music music2,
+                       @Qualifier("jazzMusic") Music music3) {
+        this.music1 = music1;
+        this.music2 = music2;
+        this.music3 = music3;
     }
 
     public String playMusic() {
-        return "Playing: " + rockMusic.getSong();
+        return "Playing: " + music1.getSong() + ", " + music2.getSong() + ", " + music3.getSong();
     }
 
 }
